4daa653a6871e3cf6d123676febd45061a1399e2
Regraz <stanma@qq.com>
2018-10-21 13:28:10 +0800

    update heatmap format

--- pattern_heatmap.m
+++ pattern_heatmap.m
@@ -1,6 +1,6 @@
 %Draw 1-D heatmap of operands. 
-%function [selected_operands] = pattern_heatmap(pattern, count, xlabel, ylabel, title, rangecnt)
-function [selected_operands] = pattern_heatmap(filepath, count, x_axis_label, y_axis_label, hmap_title, rangecnt)
+%function [selected_operands] = pattern_heatmap(filepath, count, rangecnt, x_axis_label, y_axis_label, hmap_title)
+function [selected_operands] = pattern_heatmap(filepath, count, rangecnt, x_axis_label, y_axis_label, hmap_title)
 format long;
 pattern_str=textread(filepath,'%s');
 pattern=ones(size(pattern_str,1),1);
@@ -27,7 +27,7 @@ end
 % hist(selected_operands,100);
 % xlabel("Range of Operands (Float) / Range Length: 0.02");
 % ylabel("Number of Operands in Each Range")
-% title("Distribution of 2000 Randomly Selected Multiply Operands, Layer 1");
+% title("Distribution of 2000 Randomly Selected Multiply Ope rands, Layer 1");
 
 % subplot(2,2,3);
 % hist(selected_operands,100);
@@ -41,7 +41,9 @@ end
 % ylabel("Number of Operands in Each Range")
 % title("Distribution of 2000 Randomly Selected Multiply Operands, Layer 3");
 
-hist(selected_operands,rangecnt);
+[ntemp,xotemp]=hist(selected_operands,rangecnt);
+ntemp=ntemp/sum(ntemp);
+bar(xotemp, ntemp);
 xlabel(x_axis_label);
 ylabel(y_axis_label)
 title(hmap_title);
